8a351064bf4c305ece6d2b48ec14bb6dce3290e7
Franznic0 <franznicofranz@gmail.com>
2024-03-29 10:54:47 +0000

    Upload proj

---
+++ app.js
@@ -0,0 +1,19 @@
+const show = () => {
+    var accordion = document.querySelectorAll('.accordion');
+
+    for(let i = 0; i < accordion.length ; i++) {
+        accordion[i].addEventListener('click', function () {
+
+            //toggle accordion
+            this.classList.toggle('active');
+            
+            // show answer
+            let answer = this.nextElementSibling;
+            console.log(answer);
+            answer.classList.toggle('active');
+        });
+    };
+    
+};
+
+show();
